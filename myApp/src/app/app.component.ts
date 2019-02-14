2abef0d0e82e0b9ee7cada405b349e537c83770d
Manish <manishkumar1725@gmail.com>
2019-02-14 16:55:27 +0530

    first commit

---
+++ myApp/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'Welcome';
+  firstName = 'Manish';
+  lastName = 'Kumar';
+
+  location : Address = 
+  {
+    	    "street" : "Bangalore",
+    	    "city" : "Karnataka"
+  };
+}
+
+  class Address {
+    street : string;
+    city : string;
+  }
+    
+
+
+  
